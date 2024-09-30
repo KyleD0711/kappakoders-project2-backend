47f539e31e399eafbb29ce124f4c2cae88e024b8
KyleD0711 <kyle.denney@eagles.oc.edu>
2024-09-30 09:58:11 -0500

    Did stuff

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASS,
-  DB: "course",
+  PASSWORD: process.env.DB_PW,
+  DB: process.env.DB_NAME,
   dialect: "mariadb",
   dialectOptions: {
     allowPublicKeyRetrieval: true,
